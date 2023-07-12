a82d32ad78743a84b0c81dbff62260e1a9cdffab
arikatlavamsi478 <arikatlavamsi478@users.noreply.github.com>
2023-07-12 12:59:29 -0700

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-// run `node index.js` in the terminal
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true,
+});
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Declare a route
+//fastify.get("/", const handler = (request, reply) =>
+// reply.send({ hello: "world" });
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' });
+});
+
+fastify.get('/api/healthchecker', (request, reply) => {
+  reply.status(200).send({
+    //reply.status(200).send({
+    status: 'success',
+    message: 'Build CRUD API with Node.js and Sequelize',
+  });
+});
+
+fastify.all('*', (request, reply) => {
+  reply.status(404).send({
+    status: 'fail',
+    // message: "Route does not exist on this server",
+    // message: `Route: ${request.raw.url} does not exist on this server`,
+    message: `Route: ${request.raw.url} does   not exist on this server`,
+  });
+});
+// Run the server!
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  //console.log("server is now listening on ${address}");
+  fastify.log.info(`server listening on ${address}`);
+});
